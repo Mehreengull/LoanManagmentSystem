fd2c8e5da09e3b1635b86de67be512b06c531830
Mehreengul <gmehreen06@gmail.com>
2019-04-18 23:18:02 +0500

    Front End

---
+++ F1/F1/Models/AccountViewModels.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace F1.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth date")]
+        public string DOB { get; set; }
+
+     
+        [Display(Name = "State")]
+        public string state { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string city { get; set; }
+
+        [Display(Name = "Province")]
+        public string Province { get; set; }
+        [Required]
+        [Display(Name = "Salary")]
+        public string salary { get; set; }
+        [Required]
+        [Display(Name = "Gender")]
+        public string gender { get; set; }
+        [Required]
+        [Display(Name = "Status")]
+        public string status { get; set; }
+        [Required]
+        [Display(Name = "Rank")]
+        public string rank { get; set; }
+        [Required]
+        [Display(Name = "Type")]
+        public string type { get; set; }
+        [Required]
+        [Display(Name = "Department ID")]
+        public string DID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Joining")]
+        public string DOJ { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Ending")]
+        public string EndDate { get; set; }
+
+
+
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ F1/F1/Models/option.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace F1.Models
+{
+    public class option
+    {
+        public string admin { get; set; }
+        public string user { get; set; }
+    }
+}
