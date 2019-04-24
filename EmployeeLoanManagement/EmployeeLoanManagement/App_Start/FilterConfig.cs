48bd6774a23d938bfa9e7982a35e77347e464062
2016CS101 <2016cs101@student.uet.edu.pk>
2019-04-23 22:43:35 -0700

    Project Initialized

---
+++ EmployeeLoanManagement/EmployeeLoanManagement/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeLoanManagement
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ EmployeeLoanManagement/EmployeeLoanManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeLoanManagement.MvcApplication" Language="C#" %>
