293bd4142f8d4abe6f7211bf7bfc308edf3dbf97
2016CS101 <2016cs101@student.uet.edu.pk>
2019-05-16 12:07:08 -0700

    Project Completed

---
+++ EMSProj/EMSProj/CollectionViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMSProj.CollectionViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public List<Department> Departments { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+        public List<Rank> Ranks { get; set; }
+        public IEnumerable<Loan> Loans { get; set; }
+        
+        public Department Department { get; set; }
+        public Employee Employee { get; set; }
+        public Rank rank { get; set; }
+        
+   
+    }
+}
---
+++ EMSProj/EMSProj/CollectionViewModels/EmployeeLoanCollectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMSProj.CollectionViewModels
+{
+    public class EmployeeLoanCollectionViewModel
+    {
+        public Employee Employee { get; set; }
+        public Loan Loan { get; set; }
+    }
+}
